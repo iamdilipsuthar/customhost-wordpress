2898e92facf6521cf6a66f0fec0f81e8eee6a065
John <eyedonz@gmail.com>
2019-03-20 19:59:48 +0530

    load style file from main plugin file

--- customhost-wordpress.php
+++ customhost-wordpress.php
@@ -4,3 +4,12 @@ Plugin Name: Customhost-wordpress
 Description: This is just simple plugin with github repo.
 Author: Eyedo
 */
+
+
+
+add_action('wp_enqueue_scripts',function(){
+
+  wp_register_style('custom-style',plugins_url('assets/main.css',__FILE__),'1.0');
+  wp_enqueue_style('custom-style');
+
+},100);
