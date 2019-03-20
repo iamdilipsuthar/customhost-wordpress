279132e7640c84e53f9e3bc2d241ec73d56a023b
John <eyedonz@gmail.com>
2019-03-20 23:05:39 +0530

    new custom script file loaded to admin side

--- customhost-wordpress.php
+++ customhost-wordpress.php
@@ -5,12 +5,16 @@ Description: This is just simple plugin with github repo.
 Author: Eyedo
 */
 
-
+//load asset to frontend side
 function my_custom_styles() {
-  wp_register_style( 'custom-styles',plugins_url('assets/main.css',__FILE__));
-  wp_enqueue_style( 'custom-styles' );
-
   wp_register_script('custom-script',plugins_url('assets/main.js',__FILE__),array('jquery'),'1.0');
   wp_enqueue_script('custom-script');
 }
 add_action( 'wp_enqueue_scripts', 'my_custom_styles' , 100);
+
+//load asset to admin side
+function my_custom_styles2() {
+  wp_register_script('admin-script',plugins_url('assets/admin/main.js',__FILE__),array('jquery'),'1.0');
+  wp_enqueue_script('admin-script');
+}
+add_action( 'admin_enqueue_scripts', 'my_custom_styles2' , 100);
